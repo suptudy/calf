1ae1d0b2b1a18c2d9cdd1ff82ad6bc23482a6cef
Subin Kwon <rnjstnqls99@naver.com>
2023-03-17 02:27:16 +0000

    leg Image Processing and multi drag and drop file

--- app.py
+++ app.py
@@ -80,9 +80,11 @@ if choose == "Board Pixel CSV file":
 # Leg Image Processing
 if choose == "Leg Image Processing":
     st.title('Leg Image Processing') 
-    st.caption('이미지를 앞면, (피사체 기준) 오른쪽 다리, (피사체 기준) 왼쪽 다리 순으로 넣어주세요.')
+    st.markdown(""" 
+    한 명에 대한 다리 이미지를 넣어주세요
+    """)
 
-    uploaded_files = st.file_uploader("한 명에 대한 다리 이미지를 넣어주세요.", type=['png','jpg'], accept_multiple_files=True)
+    uploaded_files = st.file_uploader(label=" ", type=['png','jpg'], accept_multiple_files=True)
     
     col1, col2, col3 = st.columns(3)
     real_image_list = []
@@ -104,9 +106,17 @@ if choose == "Leg Image Processing":
         # u2-net
         remove_image = remove(real_image)
         
-        # 최종 이미지들 list화
-        real_image_list.append(real_image)
-        remove_image_list.append(remove_image)
+        # 최종 이미지들 list화 : 이미지 순서 상관없이 drag, drop 가능 
+        check_append_order = uploaded_file.name[-5:-4]
+        if check_append_order == 'f':
+            real_image_list.insert(0, real_image)
+            remove_image_list.insert(0, remove_image)
+        elif check_append_order == 'r':
+            real_image_list.insert(1, real_image)
+            remove_image_list.insert(1, remove_image)
+        elif check_append_order == 'l':
+            real_image_list.insert(2, real_image)
+            remove_image_list.insert(2, remove_image)
         
     # read board pixel front 
     df_front = pd.read_csv('./board_pixel - front.csv') 
@@ -212,45 +222,47 @@ if choose == "Leg Image Processing":
 if choose == "Estimate Calf Round":
     st.title('Estimate Calf Round')
     st.markdown(
-    """종아리 왼쪽, 오른쪽에 상관 없이, **mm 단위**로 입력해주세요
+    """종아리 왼쪽, 오른쪽에 상관 없이, **mm 단위**로 입력해주세요 \n
+        
     """
     )
-    no_button = st.button("Leg Image Processing 과정 :x:")
-    yes_button = st.button("Leg Image Processing 과정 :o:")
     
-    if no_button:
+    if not os.path.isfile('thick_final_result1.csv'):
         with st.container():
-            st.subheader('(수정필요) Leg Image Processing 과정 없이 직접 입력')
+            st.subheader('Leg Image Processing 과정 없이 직접 입력')
             
             st.subheader("앞면 width (mm)")
             frontNum = st.number_input(label='Insert a number', key='1', format='%d', step=1)
             st.subheader("옆면 width (mm)")
             sideNum = st.number_input(label='Insert a number', key='2', format='%d', step=1)
 
-            st.write('The current number is ', frontNum)
-            st.write('The current number is ', sideNum)
+            st.write('앞면 width에 대한 값은 ', frontNum)
+            st.write('옆면 width에 대한 값은 ', sideNum)
             
-    elif yes_button:
+    else :
         with st.container():
             st.subheader('Leg Image Processing 진행을 했을 경우')
             
-            # thick_final_result 유/무에 따라 
-            if os.path.isfile('thick_final_result.csv'):
-                df = pd.read_csv('thick_final_result.csv')
+            df = pd.read_csv('thick_final_result.csv')
+            st.dataframe(df)
                 
-                col3, col4 = st.columns(2)
-                with col3:
-                    st.subheader("앞면 width (mm)")
-                    st.write('오른쪽 앞면 width에 대한 값은 ', df['front_thick_width'][0])
-                    st.write('왼쪽 앞면 width에 대한 값은 ', df['front_thick_width'][1])
-                with col4:
-                    st.subheader("옆면 width (mm)")
-                    st.write('오른쪽 옆면 width에 대한 값은 ', df['side_thick_width'][0])
-                    st.write('왼쪽 옆면 width에 대한 값은 ', df['side_thick_width'][1])
-            else :
-                st.write('Leg Image Processing 과정을 거치면 자동으로 입력됩니다.')
-                st.write('Leg Image Processing 과정을 거치거나, **[Leg Image Processing 과정 X]** 을 눌러 직접 입력하세요.')
+            col3, col4 = st.columns(2)
+                
+            with col3:
+                st.subheader("앞면 width (mm)")
+                st.write('오른쪽 앞면 width에 대한 값은 ', df['front_thick_width'][0])
+                st.write('왼쪽 앞면 width에 대한 값은 ', df['front_thick_width'][1])
+            with col4:
+                st.subheader("옆면 width (mm)")
+                st.write('오른쪽 옆면 width에 대한 값은 ', df['side_thick_width'][0])
+                st.write('왼쪽 옆면 width에 대한 값은 ', df['side_thick_width'][1])
 
+    # model
+    st.markdown("""---""")
+    st.subheader("종아리 둘레 예측 결과")
+    check_result = st.button("확인")
+    if check_result:
+        st.write('test')
         
 # Guide
 if choose == "Guide":
@@ -261,31 +273,48 @@ if choose == "Guide":
     """
     )
     
-    img = Image.open('photoInfo.png') # 고화질로 수정할 것 
+    img = Image.open('guide_img/photoInfo.png') 
     st.image(img) 
     
     st.markdown(
     """
-    - 폼보드에 종아리가 다 들어와야하며, 최대한 옷이 나오지 않아야 한다.
-    - 옆면 촬영 시, 다른 쪽의 다리가 보이지 않도록 촬영해야 한다. 
+    - 폼보드(흰색)에 종아리가 다 들어와야하며, 최대한 옷이 나오지 않아야 합니다. 
+    - 옆면 촬영 시, 다른 쪽의 다리가 보이지 않도록 촬영해야 합니다.
     
     ### :pushpin: 필요한 데이터
+    """)
+    
+    img = Image.open('guide_img/nameInfo.png') 
+    st.image(img, width=500, caption="각 이미지별 이름 예시 (001 : 사람 ID)", output_format='PNG') 
+    
+    st.markdown(
+    """
+    - 종아리의 왼쪽, 오른쪽의 기준은 피사체 기준입니다. 
     - 한 명에 대한 종아리 이미지 **[ 앞면, 옆면(오른쪽), 옆면(왼쪽) ]**
+        - 이미지의 이름 끝에 반드시 앞면은 **f**, 옆면(오른쪽)은 **r**, 옆면(왼쪽)은 **l**이 들어가야 합니다. 
+        - 예시) 001_f.jpg, 001_r.jpg, 001_l.jpg
+        
     - 폼보드 각 모서리의 픽셀값
     
     ### :pushpin: Calf Program 유의사항
-    - 페이지를 이동하면 결과가 사라집니다 (ex. Leg Image Processing 페이지에서 Regression Model 페이지로 넘어갔다가 다시 돌아가면 초기 상태로 돌아갑니다.)
+    - 페이지를 이동하면 결과가 사라집니다 
+        - ex. Leg Image Processing 페이지에서 Estimate Calf Round 페이지로 넘어갔다가 다시 돌아가면 초기 상태로 돌아갑니다. \n 
+            앞에서 실행했던 Leg Image Processing에 대한 결과는 `Leg Image Processing 과정O` 에 저장되어 있습니다. 
+    
+    ---
     
     ### :pushpin: Board Pixel CSV file
     - 폼보드에 보이는 다리만 측정할 수 있도록 폼보드의 모서리 픽셀값이 적혀있는 엑셀 파일이 필요합니다. 
     ### :pushpin: Leg Image Processing
     - 이미지에 대한 처리 과정 및 두꺼운 부분에 대한 위치, 길이를 **Final Result**에서 확인할 수 있습니다.
-    ### :pushpin: Estimate Calf Round
-    - 종아리 둘레 예측 결과를 확인하기 위해 필요한 값을 Leg Image Processing에서 확인하여 입력합니다. 
-        - 앞면 width (mm) : front thick width
-        - 옆면 width (mm) : side thick width
+    ### :pushpin: Estimate Calf Round (수정)
+    - `Leg Image Processing 과정X`
+        - 종아리 둘레 예측 결과를 확인하기 위해 필요한 값을 Leg Image Processing에서 확인하여 입력합니다. 
+            - 앞면 width (mm) : front thick width
+            - 옆면 width (mm) : side thick width
+    - `Leg Image Processing 과정O`
     
-    ##### ________________________________________________
+    ---
     ##### [Image Processing and Intelligent Systems Laboratory](https://www.ipis.cau.ac.kr/%ED%99%88)
     (Chung-Ang University, Seoul 06974, Korea)
     - Su Bin Kwon
@@ -296,6 +325,6 @@ if choose == "Guide":
     ##### ________________________________________________
 """
 )
-    file = 'thick_final_result.csv'
-    if os.path.isfile(file):
-        os.remove(file) 
+    # file = 'thick_final_result.csv'
+    # if os.path.isfile(file):
+    #     os.remove(file) 
